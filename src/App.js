7863fb60997a5b052b3d5cb4932b4e55bbc48da4
KartikC137 <119693639+KartikC137@users.noreply.github.com>
2025-03-16 16:25:39 +0530

    fresh starting new project with same dependencies

--- src/App.js
+++ src/App.js
@@ -1,14 +1,2 @@
 import "./App.css";
 import React from "react";
-import Quotes from "./components/Quotes";
-
-// imma style this later
-function App() {
-  return (
-    <div id="quote-box">
-      <Quotes />
-    </div>
-  );
-}
-
-export default App;
